e66fed72ffc116ef3e70f8c93e9156a0e0122c7d
Fabricio Kassardjian <fkay13@gmail.com>
2020-09-18 15:09:45 -0300

    Identificando todas as partes do forms na entrada da API, testes com NLU no Jupyter Notebook

--- serverless_audio_processing-master/action/action.py
+++ serverless_audio_processing-master/action/action.py
@@ -1,7 +1,8 @@
 from cgi import parse_multipart, parse_header
 from io import BytesIO
 from base64 import b64decode
-from ibm_watson import SpeechToTextV1, ApiException
+from ibm_watson import SpeechToTextV1, ApiException, NaturalLanguageUnderstandingV1
+from ibm_watson.natural_language_understanding_v1 import Features, EntitiesOptions
 from ibm_cloud_sdk_core.authenticators import BasicAuthenticator
 import json, os
 
@@ -25,50 +26,69 @@ def main(args):
         BytesIO(decoded_string), p_dict
     )
 
-    # Build flac file from stream of bytes
-    fo = open("audio_sample.flac", 'wb')
-    fo.write(multipart_data.get('audio')[0])
-    fo.close()
+    carro = ''
+    if 'car' in multipart_data:
+        carro = multipart_data.get('car')[0]
 
-    text = multipart_data.get('text')[0]
+    retorno = {
+        'recommendation': ''
+    }
 
-    # Basic Authentication with Watson STT API
-    stt_authenticator = BasicAuthenticator(
-        'apikey',
-        'qa8K2ibwTd2CoJAvrUfhSNaZL2nlrIjaafpjTxslF30C'
-    )
+    texto_audio = ''
 
-    # Construct a Watson STT client with the authentication object
-    stt = SpeechToTextV1(
-        authenticator=stt_authenticator
-    )
+    if 'audio' in multipart_data:
+        # Build flac file from stream of bytes
+        fo = open("audio_sample.flac", 'wb')
+        fo.write(multipart_data.get('audio')[0])
+        fo.close()
 
-    # Set the URL endpoint for your Watson STT client
-    stt.set_service_url(
-        'https://api.us-south.speech-to-text.watson.cloud.ibm.com'
-    )
+    
 
-    # Read audio file and call Watson STT API:
-    with open(
-        os.path.join(
-            os.path.dirname(__file__), './.',
-            'audio_sample.flac'
-        ), 'rb'
-    ) as audio_file:
-        # Transcribe the audio.flac with Watson STT
-        # Recognize method API reference: 
-        # https://cloud.ibm.com/apidocs/speech-to-text?code=python#recognize
-        stt_result = stt.recognize(
-            audio=audio_file,
-            content_type='audio/flac',
-            model='pt-BR_BroadbandModel'
-        ).get_result()
+        # Basic Authentication with Watson STT API
+        stt_authenticator = BasicAuthenticator(
+            'apikey',
+            'qa8K2ibwTd2CoJAvrUfhSNaZL2nlrIjaafpjTxslF30C'
+        )
 
-    # Print STT API call results
-    print(json.dumps(stt_result, indent=2))
+        # Construct a Watson STT client with the authentication object
+        stt = SpeechToTextV1(
+            authenticator=stt_authenticator
+        )
+
+        # Set the URL endpoint for your Watson STT client
+        stt.set_service_url(
+            'https://api.us-south.speech-to-text.watson.cloud.ibm.com'
+        )
+
+        # Read audio file and call Watson STT API:
+        with open(
+            os.path.join(
+                os.path.dirname(__file__), './.',
+                'audio_sample.flac'
+            ), 'rb'
+        ) as audio_file:
+            # Transcribe the audio.flac with Watson STT
+            # Recognize method API reference: 
+            # https://cloud.ibm.com/apidocs/speech-to-text?code=python#recognize
+            stt_result = stt.recognize(
+                audio=audio_file,
+                content_type='audio/flac',
+                model='pt-BR_BroadbandModel'
+            ).get_result()
+
+        # Print STT API call results
+        print(json.dumps(stt_result, indent=2))
+        texto_audio = stt_result['results'][0]['alternatives'][0]['transcript']
+
+    # pega o texto da mensagem
+    texto_forms = 'Sem texto'
+    if('text' in multipart_data):
+        texto_forms = multipart_data.get('text')[0]
+
 
     # Return a dictionary with the transcribed text
     return {
-        "transcript": stt_result['results'][0]['alternatives'][0]['transcript'],
-        "text": text
+        "car": carro,
+        "transcript": texto_audio,
+        "texto": texto_forms
     }
